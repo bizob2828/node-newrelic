d0a360b85491ad1267977f90334392483b1fa677
Bob Evans <robert.evans25@gmail.com>
2022-09-21 11:55:08 -0400

    WIP: show how moving .mjs to .js will work when using fixes to eslint-plugin-node

---
+++ test/versioned/grpc-esm/.eslintrc.cjs
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2022 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+module.exports = {
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: '2020'
+  }
+}
